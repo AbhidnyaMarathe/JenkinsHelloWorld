4db51dbe48c3bc5c3054a3531a3926bbcfa0a1eb
Abhidnya <abhidnyamarathe11@gmail.com>
2020-11-21 13:06:32 +0530

    HelloWolrd updated

--- HelloWorld.java
+++ HelloWorld.java
@@ -1,7 +1,7 @@
 public class HelloWorld {
 
 	public static void main(String[] args) {
-		System.out.println("Welocome to Jenkins");
+		System.out.println("Welocome to Jenkins , Trying with Git and GitHub");
 	}
 
 }
